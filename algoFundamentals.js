011d88dac862380f0e904ad1b3ba7e489e30dfce
Yasir Zafar <yzyasir@yahoo.com>
2021-03-25 22:41:01 -0400

    Just doing some string refreshers in JS

---
+++ algoFundamentals.js
@@ -0,0 +1,19 @@
+// 1. TASK: print "Hello World"
+console.log("Hello World")
+
+// 2. console.log "Hello Noelle!" with the name in a variable
+var name1 = "Noelle"
+console.log("Hello" , name1)	// with a comma
+console.log("Hello " + name1)	// with a +
+
+// 3. console.log "Hello 42!" with the number in a variable
+var name2 = 42
+console.log( "Hello " , name2 )	// with a comma
+console.log( "Hello " + name2 )	// with a +	-- this one should give us an error 
+
+// 4. console.log "I love to eat sushi and pizza." with the foods in variables
+var fave_food1 = "sushi"
+var fave_food2 = "pizza"
+console.log( ) // DO THIS with .format()  
+console.log(`I love to eat ${fave_food1} and ${fave_food2}`) // with an f string or (template literals aka template strings)
+
